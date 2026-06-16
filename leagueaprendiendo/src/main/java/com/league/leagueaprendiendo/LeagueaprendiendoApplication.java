f82d52701abd6633770c739ee4b79b5cea9e9a96
Santiago Escobar Corredor <santiagoescobarcorredor@gmail.com>
2026-06-16 17:29:40 -0500

    Creación de proyecto Spring Boot con Maven y configuración de VSCode para Java. Se incluyen archivos de configuración, controladores y pruebas iniciales.

---
+++ leagueaprendiendo/src/main/java/com/league/leagueaprendiendo/LeagueaprendiendoApplication.java
@@ -0,0 +1,13 @@
+package com.league.leagueaprendiendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueaprendiendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueaprendiendoApplication.class, args);
+	}
+
+}
